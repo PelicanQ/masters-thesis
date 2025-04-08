42504f3520cf8b34ee8bac877d452d92eba67052
PelicanQ <77930606+PelicanQ@users.noreply.github.com>
2025-04-08 13:35:22 +0200

    dense run

--- exact/threetransmon/conv_levels.py
+++ exact/threetransmon/conv_levels.py
@@ -6,8 +6,9 @@ from jobmanager.Handler import Handler3TEnergy
 import asyncio
 import time
 
-level_select = [0, 1, 2, 3, 4, 10, 20, 30, 40, 50]
-kk = np.arange(5, 10, 1)
+level_select = np.arange(0, 30, 1)
+# level_select = [0, 1, 2, 3, 4, 10, 20, 30, 40, 50]
+kk = np.arange(5, 15, 1)
 Ej1 = 50
 Ej2 = 60
 Ej3 = 45
@@ -25,8 +26,8 @@ def save(results):
     inds = np.argsort(ks)
     ks = ks[inds]
     sortE = [Es[i] for i in inds]
-    np.save("3Tkconv_kk2", ks)
-    np.save("3Tkconv_Es2", np.array(sortE).T)
+    np.save("3Tkconv_kk3", ks)
+    np.save("3Tkconv_Es3", np.array(sortE).T)
 
 
 def local_collect():
@@ -38,11 +39,13 @@ def local_collect():
         levels = single_zz_energy(1, 1, Ej1, Ej2, Ej3, Eint12, Eint23, Eint13, k=k)
         print(f"Time: ", time.perf_counter() - t)
         Es[:, i] = levels[level_select]
-    np.save("3Tkconv_kk2", ks)
-    np.save("3Tkconv_Es2", Es)
+    np.save("3Tkconv_kk_dense", ks)
+    np.save("3Tkconv_Es_dense", Es)
 
 
 # 3T energy time: (k, laptop time): (8,2) (9,3) (10, 8) (11, 26) (12, 317)
+# 3T energy time: (k, H200 time):  (8, 0.36) (9, 0.54) (10, 0.9) (11, 1.6) (12, 2.9) (13, 5.4) (14, 9.2)
+# k=15 means ~ 30 000 x 30 000 which times 8 bytes is 7.2 GB
 # There must be some memory cieling we hit. k=12 means 15625 x 15625 total Hamil
 def collect_kk():
     H = Handler3TEnergy("http://25.9.103.201:81/3T/energy", level_select)
@@ -64,13 +67,26 @@ def collect_kk():
     save(results)
 
 
+# def deltas():
+#     kk = np.load("3Tkconv_kk2.npy")
+#     Es = np.load("3Tkconv_Es2.npy")  # along one row is E vs k
+
+#     for i in range(len(Es)):
+#         plt.plot(kk, Erel[i, :])
+#         plt.title(f"Level {level_select[i]} [Ec1]")
+#         plt.xlabel("k")
+#     plt.show()
+
+
 if __name__ == "__main__":
     local_collect()
     # kk = np.load("3Tkconv_kk2.npy")
-    # Es = np.load("3Tkconv_Es2.npy")
+    # Es = np.load("3Tkconv_Es2.npy")  # along one row is E vs k
+    # Erel = Es  # - Es[0, :]  # relative ground
+
     # for i in range(len(Es)):
     #     plt.figure()
-    #     plt.plot(kk, Es[i, :])
+    #     plt.plot(kk, Erel[i, :])
     #     plt.title(f"Level {level_select[i]} [Ec1]")
     #     plt.xlabel("k")
     # plt.show()
--- server/benchmark.py
+++ server/benchmark.py
@@ -13,6 +13,8 @@ print(f"Multiprocessors: {props['multiProcessorCount']}")
 print(f"Compute Capability: {props['major']}.{props['minor']}")
 print(f"Clock Rate: {props['clockRate'] / 1e3:.0f} MHz")
 
+print(props.keys())
+
 
 def cpu(size):
     a = np.random.rand(size, size)
@@ -24,7 +26,7 @@ def task(size):
     cp.linalg.eigh(cp.asarray(a))
 
 
-t = timeit.timeit(lambda: cpu(2000), setup=lambda: cpu(2000), number=6)
+# t = timeit.timeit(lambda: cpu(2000), setup=lambda: cpu(2000), number=6)
 # CPU 2000x2000:
 # laptop 4.1
 # H100 SXM 12.9 11.1 9.5 9.7
@@ -32,7 +34,7 @@ t = timeit.timeit(lambda: cpu(2000), setup=lambda: cpu(2000), number=6)
 
 # GPU 10'000 x 10'000
 # t = timeit.timeit(lambda: task(10000), setup=lambda: task(10000), number=6)
-print("time:", t)
+# print("time:", t)
 # desktop: 54.6 s
 # laptop 101.5 s
 # PodRun:
