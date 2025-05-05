601b9cf468db99030a90384c0483515fd2ed4b38
PelicanQ <oskarwats@hotmail.com>
2025-05-05 21:51:36 +0200

    time plot

--- exact/performance/matrix_perf.py
+++ exact/performance/matrix_perf.py
@@ -18,12 +18,25 @@ def run(A):
     vals = cp.linalg.eigh(A)
 
 
-N = 24000
-print("Size:", N)
-A = cp.random.rand(N, N)
-A = A + A.T
-t = benchmark(run, (A,), n_repeat=1, n_warmup=1)
-print(t)
-# run()
-# t = timeit.timeit(run, number=1)
-# print(t)
+def aa():
+    N = 24000
+    print("Size:", N)
+    A = cp.random.rand(N, N)
+    A = A + A.T
+    t = benchmark(run, (A,), n_repeat=1, n_warmup=1)
+    print(t)
+
+
+if __name__ == "__main__":
+    x = [1000, 5000, 7000, 10000, 13000, 15000, 17000, 20000, 22000, 24000, 25000, 26000]
+    lap = [0.064, 2.8, 6.7, 18, 39, 59, 87, 238, 543, 718, np.nan, np.nan]
+    desk = [0.044, 1.5, 3.5, 9.8, 21, 31, 99, 166, 245, 335, np.nan, np.nan]
+    rt = [0.023, 0.618, 1.6, 4.0, 7.8, 12, 17, 26, 34, 43, 49, 55]
+    h2 = [0.010, 0.19, 0.4, 0.9, 1.8, 2.5, 3.6, 5.6, 7.3, 9.2, 10, 12]
+    plt.plot(x, lap, label="laptop")
+    plt.plot(x, desk, label="desktop")
+    plt.plot(x, rt, label="5090")
+    plt.plot(x, h2, label="H200")
+    plt.legend()
+
+    plt.show()
