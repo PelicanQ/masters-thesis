408430f4590ce1634c43001909fc692b756b7eb9
PelicanQ <77930606+PelicanQ@users.noreply.github.com>
2025-05-05 21:50:29 +0200

    plotting

--- anharm/four/zzz.py
+++ anharm/four/zzz.py
@@ -1,35 +1,40 @@
-# How is the zzz123 disturbed by one extra qubit?
-from other.colormap import OrBu_colormap, my_colors
+# recreate figure 6 in ZZ paper
+from other.colormap import OrBu_colormap
 import numpy as np
 from matplotlib import pyplot as plt
-from jobmanager.util import collect_jobs
 from anharm.Hamiltonian import Hamil
 from matplotlib import colors
-from sandbox.util import make_axslid, makeslid
-import inspect
-from analysis.discover import make_hoverax_refreshable, make_hoverax, make_mesh
+from anharm.four.makezz import getextrazz13
+from analysis.discover import make_discover
 
 H = Hamil(4, 4, "4")
-e = H.zzexpr("1110")
-e = H.split_deltas(e)
+e = H.split_deltas(H.zzexpr("1110"))
 f, vars = H.lambdify_expr(e)
+# \alpha_{0}, \alpha_{1}, \alpha_{2}, g_{0,1}, g_{1,2}, g_{0,2}, g_{2,3}, \Delta_{0,1}, \Delta_{1,2}, \Delta_{2,3}
 alpha = -1
-g12 = 0.2
-g23 = 0.2
+g12 = 0.4
+g23 = 0.4
 g13 = 0.05
+g34 = 0.4
+d34 = 1.1
 
-dd13 = np.linspace(-8, 8, 400)
-o2prims = np.linspace(-10, 10, 400)
-d2prim_grid, dd13_grid = np.meshgrid(o2prims, dd13)
-d23_grid = d2prim_grid + dd13_grid / 2
+dd13 = np.linspace(-10, 10, 400)
+o2prim = np.linspace(-10, 10, 400)
+o2prim_grid, dd13_grid = np.meshgrid(o2prim, dd13)
+d23_grid = o2prim_grid + dd13_grid / 2
 d12_grid = dd13_grid - d23_grid
 
 
 @np.vectorize
 def snapto0(v):
     if np.abs(v) < 1e-5:
-        return 0
+        return 0.0
     return v
+
+
+def calc(*, g12, g23, g13, g34, d34):
+    val_mat = f(alpha, alpha, alpha, g12, g23, g13, g34, d12_grid, d23_grid, d34)
+    return snapto0(val_mat)
 
 
 norm = colors.SymLogNorm(1e-5, vmin=-1e0, vmax=1e0)
@@ -37,94 +42,15 @@ cmap = OrBu_colormap()
 
 
 def only():
-    norm = colors.SymLogNorm(1e-5, vmin=-1e0, vmax=1e0)
-    vals = f(alpha, alpha, alpha, g12, g23, g13, d12_grid, d23_grid)
-    fig, ax, c = make_hoverax(d2prim_grid, dd13_grid, vals, norm=norm, cmap=cmap)
-    ax.set_title(rf"ZZZ $g_{{12}}$={g12} $g_{{23}}$={g23} $g_{{13}}$={g13} $\alpha$={alpha} units [-$\alpha$]")
-    ax.set_xlabel(r"$\omega_2^\prime$ [-$\alpha$]")
+    ax = make_discover(
+        ["g12", "g23", "g13", "g34", "d34"], [g12, g23, g13, g34, d34], o2prim_grid, dd13_grid, calc, norm, cmap
+    )
+    # plt.pcolormesh(dd34_grid, dd13_grid, f(alpha, alpha, g13, g34, dd13_grid, dd34_grid), norm=norm, cmap=cmap)
+    ax.set_title(rf"ZZZ 4T units [-$\alpha$]")
     ax.set_ylabel(r"$\Delta_{13}$ [-$\alpha$]")
-    plt.show()
-
-
-def zzzfunctions():
-    s = H.get_subspace(3)
-
-    e1 = s.get_edge("111", "021") + s.get_all_edge_corrections("111", "021")
-    e2 = s.get_edge("111", "201") + s.get_all_edge_corrections("111", "201")
-    e3 = s.get_4loop_contraction("111", "021") + s.get_4loop_contraction("111", "201")
-    group01 = e1 + e2 + e3
-    e1 = s.get_edge("111", "120") + s.get_all_edge_corrections("111", "120")
-    e2 = s.get_edge("111", "102") + s.get_all_edge_corrections("111", "102")
-    e3 = s.get_4loop_contraction("111", "120") + s.get_4loop_contraction("111", "102")
-    group12 = e1 + e2 + e3
-    e1 = s.get_edge("111", "210") + s.get_all_edge_corrections("111", "210")
-    e2 = s.get_edge("111", "012") + s.get_all_edge_corrections("111", "012")
-    e3 = s.get_4loop_contraction("111", "210") + s.get_4loop_contraction("111", "012")
-    group02 = e1 + e2 + e3
-    group3 = s.get_3cycles("111")
-    group4 = (
-        s.get_4loop_contraction("111", "003")
-        + s.get_4loop_contraction("111", "030")
-        + s.get_4loop_contraction("111", "300")
-    )
-
-    group01 = H.split_deltas(group01)
-    group12 = H.split_deltas(group12)
-    group02 = H.split_deltas(group02)
-    group3 = H.split_deltas(group3)
-    group4 = H.split_deltas(group4)
-
-    f01, vars01 = H.lambdify_expr(group01)
-    f12, vars12 = H.lambdify_expr(group12)
-    f02, vars02 = H.lambdify_expr(group02)
-    f3, vars3 = H.lambdify_expr(group3)
-    f4, vars4 = H.lambdify_expr(group4)
-    return f01, f12, f02, f3, f4
-
-
-def decomp():
-    f01, f12, f02, f3, f4 = zzzfunctions()
-
-    def calculate(alpha, g12, g23, g13, d12, d23):
-        args = (alpha, alpha, alpha, g12, g23, g13, d12, d23)
-        return snapto0(f(*args)), f01(*args), f12(*args), f02(*args), f3(*args), f4(*args)
-
-    vals, vals01, vals12, vals02, vals3, vals4 = calculate(alpha, g12, g23, g13, d12_grid, d23_grid)
-
-    fig = plt.figure()
-    ((ax1, ax2, ax3), (ax4, ax5, ax6)) = fig.subplots(2, 3)
-    fig.suptitle(
-        rf"Grouped contributions to ZZZ $g_{{12}}$={g12} $g_{{23}}$={g23} $g_{{13}}$={g13} $\alpha$={alpha} units [-$\alpha$]"
-    )
-
-    c1 = make_hoverax(d2prim_grid, dd13_grid, vals, norm=norm, cmap=cmap, ax=ax1)
-    ax1.set_title("ZZZ")
-
-    c2 = make_hoverax(d2prim_grid, dd13_grid, vals01, norm=norm, cmap=cmap, ax=ax2)
-    ax2.set_title("01")
-
-    c3 = make_hoverax(d2prim_grid, dd13_grid, vals12, norm=norm, cmap=cmap, ax=ax3)
-    ax3.set_title("12")
-
-    c4 = make_hoverax(d2prim_grid, dd13_grid, vals02, norm=norm, cmap=cmap, ax=ax4)
-    ax4.set_title("02")
-
-    c5 = make_hoverax(d2prim_grid, dd13_grid, vals3, norm=norm, cmap=cmap, ax=ax5)
-    ax5.set_title("3loops")
-
-    c6 = make_hoverax(d2prim_grid, dd13_grid, vals4, norm=norm, cmap=cmap, ax=ax6)
-    ax6.set_title("Residual 4-contractions")
-
-    fig.colorbar(c1, ax=[ax1, ax2, ax3, ax4, ax5, ax6])
-    ax1.set_ylabel(r"$\Delta_{13}$ [-$\alpha$]")
-    ax4.set_ylabel(r"$\Delta_{13}$ [-$\alpha$]")
-    ax4.set_xlabel(r"$\omega_2^\prime$ [-$\alpha$]")
-    ax5.set_xlabel(r"$\omega_2^\prime$ [-$\alpha$]")
-    ax6.set_xlabel(r"$\omega_2^\prime$ [-$\alpha$]")
-
+    ax.set_xlabel(r"$\omega_2^\prime$ [-$\alpha$]")
     plt.show()
 
 
 if __name__ == "__main__":
     only()
-    # decomp()
